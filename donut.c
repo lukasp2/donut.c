8d075806bc1aa46098b542fd5a261d3ecfe77695
Lukas Pohlman <lukpohl3@gmail.com>
2021-10-31 02:03:51 +0200

    compiles

--- donut.c
+++ donut.c
@@ -4,67 +4,74 @@
 #include <unistd.h>
 #include "matrix_ops.h"
 
-float LIGHT_SOURCE[1][3] = {{0, 1, -1}}; // location of light source
-float DIST_SCREEN = 4; // distance from observer to the screen
-float DIST_SHAPE = 5; // distance from observer to the shape
-float R1 = 1; // circle radius
-float R2 = 2; // circle offset from origin
-
-// gets light intensity for a point
-float get_luminance(float** const shape, float u, float v) {
-    float** normal = get_normal_vector(shape, u, v);
-    return dot_product((float**)LIGHT_SOURCE, normal, 3);
-}
-
-// projects 3D point on 2D plane
-void project(float** const p) {
-    p[0][0] = DIST_SCREEN * p[0][0] / (DIST_SHAPE + p[0][2]);
-    p[0][1] = DIST_SCREEN * p[0][1] / (DIST_SHAPE + p[0][2]);
-    p[0][2] = DIST_SHAPE;
-}
-
 int main() {
+    Matrix* light_source;
+    matrix_init(light_source, 1, 3);
+    float light_source_arr[1][3] = {{0, 1, -1}}; // location of light source
+    matrix_set(light_source, light_source_arr);
+
+    float dist_screen = 4; // distance from observer to the screen
+    float dist_shape = 5; // distance from observer to the shape
+    float r1 = 1; // circle radius
+    float r2 = 2; // circle offset from origin
+
+    Matrix* torus;
+    matrix_init(torus, 1, 3);
+    
+    Matrix* normal;
+    matrix_init(normal, 1, 3);
+    
+    Matrix* rotation;
+    matrix_init(rotation, 3, 3);
+
+    // ** //
     float A = 0, B = 0;
-    float z[1760];
-    char b[1760];
+    float z_buffer[1760];
+    char frame_buffer[1760];
 
     float pi = 3.1415;
     
     printf("\x1b[2J");
 
     for (;;) {
-        memset(b, 32, 1760);
-        memset(z, 0, 7040);
-
+        memset(frame_buffer, 32, 1760);
+        memset(z_buffer, 0, 7040);
+	
         for (float u = 0; u < 2*pi; u += 0.02) {
             for (float v = 0; v < 2*pi; v += 0.007) {
 		// 1. describe the circle
-		float circle[1][3] = {{R2 + R1 * cos(u), R1 * sin(u), 0}};
+		float arr[1][3] = {{r2 + r1 * cos(u), r1 * sin(u), 0}};
+		matrix_set(torus, arr);
 		
 		// 2. rotate the circle around y by v
-		float** torus = rotate_y((float**)circle, v);
-
+		matrix_set_rotation(rotation, y, v);
+		matrix_multiply(torus, rotation, torus);
+		
 		// 3. rotate the torus around x by A and around z by B
-		torus = rotate_x(torus, A);
-		torus = rotate_z(torus, B);
+		matrix_set_rotation(rotation, x, A);
+		matrix_multiply(torus, rotation, torus);
+
+		matrix_set_rotation(rotation, z, B);
+		matrix_multiply(torus, rotation, torus);
 
 		// 4. project shape to 2D plane
-		project(torus);
-		float x = torus[0][0];
-		float y = torus[0][1];
+		matrix_project(torus, dist_screen, dist_shape);
+		float x = torus->values[0][0];
+		float y = torus->values[0][1];
+		float z = torus->values[0][2];
 		
 		// 5. get luminance
-		int lum = 8 * get_luminance(torus, u, v);
+		int lum = 8 * matrix_get_luminance(torus, light_source, normal, u, v);
 
 		// 6. store z value of what we draw
-		float D = 1 / torus[0][2];
+		float D = 1 / z;
 
 		int write_idx = x + 80 * y;
 		
 		// perimeter of view
-                if (22 > y && y > 0 && 80 > x && x > 0 && D > z[write_idx]) {
-                    z[write_idx] = D;
-                    b[write_idx] = ".,-~:;=!*#$@"[lum > 0 ? lum : 0];
+                if (22 > y && y > 0 && 80 > x && x > 0 && D > z_buffer[write_idx]) {
+                    z_buffer[write_idx] = D;
+                    frame_buffer[write_idx] = ".,-~:;=!*#$@"[lum > 0 ? lum : 0];
                 }
             }
         }
@@ -74,13 +81,18 @@ int main() {
 	// rotational movement
 	for (int k = 0; k < 1761; k++)
 	{
-            putchar(k % 80 ? b[k] : 10);
+            putchar(k % 80 ? frame_buffer[k] : 10);
             A += 0.00002;
             B += 0.00001;
         }
 	
         usleep(30000);
     }
+    
+    matrix_free(torus);
+    matrix_free(normal);
+    matrix_free(rotation);
+    matrix_free(light_source);
     
     return 0;
 }
--- matrix_ops.h
+++ matrix_ops.h
@@ -1,60 +1,109 @@
+
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <assert.h>
 
-// multiplies two matrixes m{1,2} (mxr rows, mxc columns)
-float** multiply(float** const m1, int m1r, int m1c,
-	       float** const m2, int m2r, int m2c) {
-    float* values = calloc(m1r * m2c, sizeof(float));
-    float** result = malloc(m1r * sizeof(float*));
-    
-    for (int rows = 0; rows < m1r; ++rows) {
-	for (int cols = 0; cols < m2c; ++cols) {
-	    int sum = 0;
-	    for (int k = 0; k < m1r; ++k)
-		sum += m1[rows][k] * m2[k][cols];
-	    result[rows][cols] = sum;
+enum Axis {x, y, z};
+
+struct Matrix {
+    int r;
+    int c;
+    float **values;
+};
+
+typedef float Radian;
+typedef enum Axis Axis;
+typedef struct Matrix Matrix;
+
+void matrix_init(Matrix* m, int r, int c) {
+    m->r = r;
+    m->c = c;
+    m->values = malloc(sizeof(float[r][c]));
+    assert(m->values != NULL);
+}
+
+void matrix_set(Matrix* m, float values[m->r][m->c]) {
+    for (int i = 0; i < m->r; ++i) {
+        for (int ii = 0; ii < m->c; ++ii) {
+	    m->values[i][ii] = values[i][ii];
 	}
     }
+}
+
+void matrix_free(Matrix* m) {
+    free(m->values);
+}
+
+void matrix_set_rotation(Matrix* m, Axis axis, Radian r) {
+    if ( axis == x ) {
+	float rot[3][3] = {{1, 0,       0     },
+			   {0, cos(r),  sin(r)},
+			   {0, -sin(r), cos(r)}};
+	matrix_set(m, rot);
+    }
+    else if ( axis == y ) {
+    	float rot[3][3] = {{cos(r),  0, sin(r)},
+			   {0,       1, 0     },
+			   {-sin(r), 0, cos(r)}};
+	matrix_set(m, rot);
+    }
+    else {
+    	float rot[3][3] = {{cos(r),  sin(r), 0},
+			   {-sin(r), cos(r), 0},
+			   {0,       0,      1}};
+	matrix_set(m, rot);
+    }
+}
+
+// multiplies two matrixes m{1,2}
+void matrix_multiply(const Matrix* const m1, const Matrix* const m2, Matrix* result) {
+    assert(m1->c == m2->r);
     
-    return result;
-}
+    for (int r = 0; r < m1->r; ++r) {
+	for (int c = 0; c < m2->c; ++c) {
+	    int sum = 0;
+	    for (int k = 0; k < m1->c; ++k)
+		sum += m1->values[r][k] * m2->values[k][c];
+	    result->values[r][c] = sum;
+	}
+    }
 
+}
 // calculates the dot product of v1 and v2 of size k
-float dot_product(float** const v1, float** const v2, int k) {
+float matrix_get_dot_product(const Matrix* const m1, const Matrix* const m2) {
+    assert(m1->c == m2->c);
+    
     float result = 0.0;
-    for (int i = 0; i < k; i++)
-        result += v1[0][i] * v2[0][i];
+    for (int i = 0; i < m1->c; i++)
+        result += m1->values[0][i] * m2->values[0][i];
     return result;
-}
-
-// rotates point p by r around x
-float** rotate_x(float** p, float r) {
-    float m[3][3] = {{1, 0,       0     },
-		     {0, cos(r),  sin(r)},
-		     {0, -sin(r), cos(r)}};
-    return multiply(p, 1, 3, (float**)m, 3, 3);
-}
-
-// rotates point p by r around y
-float** rotate_y(float** p, float r) {
-    float m[3][3] = {{cos(r),  0, sin(r)},
-		     {0,       1, 0     },
-		     {-sin(r), 0, cos(r)}};
-    return multiply(p, 1, 3, (float**)m, 3, 3);
-}
-
-// rotates point p by r around z
-float** rotate_z(float** p, float r) {
-    float m[3][3] = {{cos(r),  sin(r), 0},
-		     {-sin(r), cos(r), 0},
-		     {0,       0,      1}};
-    return multiply(p, 1, 3, (float**)m, 3, 3);
 }
 
 // gets a normal vector for a point on the shape given u, v
-float** get_normal_vector(float** shape, float u, float v) {
-    float point[1][3] = {{cos(u), sin(u), 0}};
-    float** normal_v = multiply((float**)point, 1, 3, shape, 3, 3);
-    return normal_v;
+void matrix_set_normal(const Matrix* const m, float u, float v, Matrix* normal) {
+    Matrix* m2;
+    matrix_init(m2, 1, 3);
+    
+    float f[1][3] = {{cos(u), sin(u), 0}};
+    matrix_set(m2, f);
+
+    matrix_multiply(m2, m, normal);
+
+    matrix_free(m2);
 }
+
+// projects 3D point on 2D plane
+void matrix_project(const Matrix* const m, float dist_screen, float dist_shape) {
+    m->values[0][0] = dist_screen * m->values[0][0] / (dist_shape + m->values[0][2]);
+    m->values[0][1] = dist_screen * m->values[0][1] / (dist_shape + m->values[0][2]);
+}
+
+// gets light intensity for a point
+float matrix_get_luminance(const Matrix* const m,
+			   const Matrix* const light_source,
+			   Matrix* const normal, float u, float v) {
+    matrix_set_normal(m, u, v, normal);
+    return matrix_get_dot_product(light_source, normal);
+}
+
